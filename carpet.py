6fef2736968e32998c0236d4a1d56d1a3720c3aa
LaVieEstDure <mishraraghav24@gmail.com>
2018-03-07 21:23:25 +1000

    Initial files

---
+++ carpet.py
@@ -0,0 +1,8 @@
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="CLI interface for carpet")
+    parser.add_argument("A cargo-like utility to manage virtual environments "
+                        "and dependencies in Python, in a natural and logical"
+                        "manner")
+    
